0342e16753806745d46609f0f7f8caf452ac66e6
Bihesha <bihesha0822@gmail.com>
2024-10-06 19:17:13 +0530

    initial commit.

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EAD_Ecommerce_Mobile_new"
+include(":app")
+ 
